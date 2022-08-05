29b95c775eb4317bc8a6029bda4c53c022d67e7a
dasx10 <dasx10@gmail.com>
2022-08-05 22:18:01 +0300

    types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,30 @@
-export type CurryLimitReverse <> = ;
+import type { CurryLimitReverseCore } from 'fup-curry-limit-reverse-core';
+import type CurryLimitResultType      from 'fup-curry-limit-result-type';
+import type TupleConsistentType       from 'fup-tuple-consistent-type';
+import type TupleGainType             from 'fup-tuple-gain-type';
+import type TupleReverseType          from 'fup-tuple-reverse-type';
+
+export type CurryLimitReverse <
+    ExpectedParameters extends readonly unknown[] = unknown[],
+    ExpectedResult     extends unknown = unknown,
+    ExpectedLimit      extends number  = number
+> = CurryLimitReverseCore<ExpectedParameters, ExpectedResult, ExpectedLimit> & {
+
+    <Limit extends ExpectedLimit>(limit: Limit): {
+        <
+            Parameters  extends ExpectedParameters,
+            Result      extends ExpectedResult,
+            Arguments   extends TupleConsistentType<TupleGainType<Limit, TupleReverseType<Parameters>>>
+        >(executor: (...parameters: Parameters) => Result, ...arguments: Arguments): CurryLimitResultType<
+            Limit,
+            Arguments,
+            TupleReverseType<Parameters>,
+            Result
+        >;
+    };
+
+    (): CurryLimitReverse<ExpectedParameters, ExpectedResult>;
+}
 
 /**
  * 
